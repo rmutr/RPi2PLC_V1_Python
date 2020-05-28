9a2e65c3801e599ddb5451d32590cc56ccaab588
isara1120 <isara11200@gmail.com>
2020-05-28 14:29:07 +0700

    update jobsen_lab2

--- jobsen_lab2.py
+++ jobsen_lab2.py
@@ -15,6 +15,7 @@ class R_DI_NPN:
   def __init__(self, di_address, di_bit): 
     self.di_address = di_address 
     self.di_bit = di_bit 
+    self.value = 0 
 
 class R_DO_NPN: 
   def __init__(self, do_address, do_bit): 
@@ -66,6 +67,42 @@ do_byte.append(0)
 do_byte.append(0) 
 do_byte.append(0) 
 
+di.append(R_DI_NPN(0x20, 0)) 
+di.append(R_DI_NPN(0x20, 1)) 
+di.append(R_DI_NPN(0x20, 2)) 
+di.append(R_DI_NPN(0x20, 3)) 
+di.append(R_DI_NPN(0x20, 4)) 
+di.append(R_DI_NPN(0x20, 5)) 
+di.append(R_DI_NPN(0x20, 6)) 
+di.append(R_DI_NPN(0x20, 7)) 
+
+di.append(R_DI_NPN(0x21, 0)) 
+di.append(R_DI_NPN(0x21, 1)) 
+di.append(R_DI_NPN(0x21, 2)) 
+di.append(R_DI_NPN(0x21, 3)) 
+di.append(R_DI_NPN(0x21, 4)) 
+di.append(R_DI_NPN(0x21, 5)) 
+di.append(R_DI_NPN(0x21, 6)) 
+di.append(R_DI_NPN(0x21, 7)) 
+
+di.append(R_DI_NPN(0x22, 0)) 
+di.append(R_DI_NPN(0x22, 1)) 
+di.append(R_DI_NPN(0x22, 2)) 
+di.append(R_DI_NPN(0x22, 3)) 
+di.append(R_DI_NPN(0x22, 4)) 
+di.append(R_DI_NPN(0x22, 5)) 
+di.append(R_DI_NPN(0x22, 6)) 
+di.append(R_DI_NPN(0x22, 7)) 
+
+di.append(R_DI_NPN(0x23, 0)) 
+di.append(R_DI_NPN(0x23, 1)) 
+di.append(R_DI_NPN(0x23, 2)) 
+di.append(R_DI_NPN(0x23, 3)) 
+di.append(R_DI_NPN(0x23, 4)) 
+di.append(R_DI_NPN(0x23, 5)) 
+di.append(R_DI_NPN(0x23, 6)) 
+di.append(R_DI_NPN(0x23, 7)) 
+
 do.append(R_DO_NPN(0x20, 0)) 
 do.append(R_DO_NPN(0x20, 1)) 
 do.append(R_DO_NPN(0x20, 2)) 
@@ -109,15 +146,123 @@ def sys_communication():
   return 0
 
 def sys_read_digital_inputs(): 
+  for bdi in di: 
+    bdi.value = 0 
+
+  di_byte[0] = (255 - hw_do.read_byte(di[0].di_address)) 
+
+  if (di_byte[0] & 0x01) > 0: 
+    di[0].value = 1 
+
+  if (di_byte[0] & 0x02) > 0: 
+    di[1].value = 1 
+
+  if (di_byte[0] & 0x04) > 0: 
+    di[2].value = 1 
+
+  if (di_byte[0] & 0x08) > 0: 
+    di[3].value = 1 
+
+  if (di_byte[0] & 0x10) > 0: 
+    di[4].value = 1 
+
+  if (di_byte[0] & 0x20) > 0: 
+    di[5].value = 1 
+
+  if (di_byte[0] & 0x40) > 0: 
+    di[6].value = 1 
+
+  if (di_byte[0] & 0x80) > 0: 
+    di[7].value = 1 
+
+  #di_byte[1] = (255 - hw_do.read_byte(di[8].di_address)) 
+
+  if (di_byte[1] & 0x01) > 0: 
+    di[8].value = 1 
+
+  if (di_byte[1] & 0x02) > 0: 
+    di[9].value = 1 
+
+  if (di_byte[1] & 0x04) > 0: 
+    di[10].value = 1 
+
+  if (di_byte[1] & 0x08) > 0: 
+    di[11].value = 1 
+
+  if (di_byte[1] & 0x10) > 0: 
+    di[12].value = 1 
+
+  if (di_byte[1] & 0x20) > 0: 
+    di[13].value = 1 
+
+  if (di_byte[1] & 0x40) > 0: 
+    di[14].value = 1 
+
+  if (di_byte[1] & 0x80) > 0: 
+    di[15].value = 1 
+
+  #di_byte[2] = (255 - hw_do.read_byte(di[16].di_address)) 
+
+  if (di_byte[2] & 0x01) > 0: 
+    di[16].value = 1 
+
+  if (di_byte[2] & 0x02) > 0: 
+    di[17].value = 1 
+
+  if (di_byte[2] & 0x04) > 0: 
+    di[18].value = 1 
+
+  if (di_byte[2] & 0x08) > 0: 
+    di[19].value = 1 
+
+  if (di_byte[2] & 0x10) > 0: 
+    di[20].value = 1 
+
+  if (di_byte[2] & 0x20) > 0: 
+    di[21].value = 1 
+
+  if (di_byte[2] & 0x40) > 0: 
+    di[22].value = 1 
+
+  if (di_byte[2] & 0x80) > 0: 
+    di[23].value = 1 
+
+  #di_byte[3] = (255 - hw_do.read_byte(di[24].di_address)) 
+
+  if (di_byte[3] & 0x01) > 0: 
+    di[24].value = 1 
+
+  if (di_byte[3] & 0x02) > 0: 
+    di[25].value = 1 
+
+  if (di_byte[3] & 0x04) > 0: 
+    di[26].value = 1 
+
+  if (di_byte[3] & 0x08) > 0: 
+    di[27].value = 1 
+
+  if (di_byte[3] & 0x10) > 0: 
+    di[28].value = 1 
+
+  if (di_byte[3] & 0x20) > 0: 
+    di[29].value = 1 
+
+  if (di_byte[3] & 0x40) > 0: 
+    di[30].value = 1 
+
+  if (di_byte[3] & 0x80) > 0: 
+    di[31].value = 1 
+
+
+
   return 0 
 
 def sys_read_analog_inputs(): 
   return 0 
 
 def sys_process(): 
-  for bdo in do: 
-    bdo.toggle() 
-  
+  #for bdo in do: 
+  #  bdo.toggle() 
   return 0
 
 def sys_write_digital_outputs(): 
@@ -239,7 +384,7 @@ def sys_write_analog_output():
   return 0 
 
 def sys_display():
-  print("Run..") 
+  print(str(di[7].value) + str(di[6].value) + str(di[5].value) + str(di[4].value) + " " + str(di[3].value) + str(di[2].value) + str(di[1].value) + str(di[0].value)) 
   return 0 
 
 def sys_time_controller():
