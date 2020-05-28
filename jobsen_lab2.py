b201da908419ff351792a21426596e214616020e
isara1120 <isara11200@gmail.com>
2020-05-28 13:38:19 +0700

    Update jobsen_lab2.py.

--- jobsen_lab2.py
+++ jobsen_lab2.py
@@ -22,17 +22,12 @@ class R_DO_NPN:
     self.do_bit = do_bit 
     self.value = 0 
 
-  def update(self): 
-    return 0 
-
   def off(self): 
     self.value = 0 
-    self.update() 
     return 0 
 
   def on(self): 
     self.value = 1 
-    self.update() 
     return 0 
 
   def toggle(self): 
@@ -40,7 +35,8 @@ class R_DO_NPN:
       self.value = 1 
     else: 
       self.value = 0 
-    self.update() 
+
+    #self.value != self.value 
     return 0 
 
 class R_ADC: 
@@ -57,6 +53,18 @@ di = [] #<- faster than di = list()
 do = [] 
 ai = [] 
 ao = [] 
+
+di_byte = [] 
+di_byte.append(0)
+di_byte.append(0)
+di_byte.append(0)
+di_byte.append(0)
+
+do_byte = [] 
+do_byte.append(0) 
+do_byte.append(0) 
+do_byte.append(0) 
+do_byte.append(0) 
 
 do.append(R_DO_NPN(0x20, 0)) 
 do.append(R_DO_NPN(0x20, 1)) 
@@ -107,37 +115,123 @@ def sys_read_analog_inputs():
   return 0 
 
 def sys_process(): 
-  do[0].toggle() 
+  for bdo in do: 
+    bdo.toggle() 
+  
   return 0
 
 def sys_write_digital_outputs(): 
-  value_byte = 0 
+  do_byte[0] = 0 
 
   if do[0].value == 1: 
-    value_byte += 1 
+    do_byte[0] += 1 
 
   if do[1].value == 1 : 
-    value_byte += 2 
+    do_byte[0] += 2 
 
   if do[2].value == 1: 
-    value_byte += 4 
+    do_byte[0] += 4 
 
   if do[3].value == 1: 
-    value_byte += 8 
+    do_byte[0] += 8 
 
   if do[4].value == 1: 
-    value_byte += 16 
+    do_byte[0] += 16 
 
   if do[5].value == 1: 
-    value_byte += 32 
+    do_byte[0] += 32 
 
   if do[6].value == 1: 
-    value_byte += 64 
+    do_byte[0] += 64 
 
   if do[7].value == 1: 
-    value_byte += 128 
+    do_byte[0] += 128 
 
-  hw_do.write_byte(do[0].do_address, (255 - value_byte)) 
+  hw_do.write_byte(do[0].do_address, (255 - do_byte[0])) 
+
+  do_byte[1] = 0 
+
+  if do[8].value == 1: 
+    do_byte[1] += 1 
+
+  if do[9].value == 1 : 
+    do_byte[1] += 2 
+
+  if do[10].value == 1: 
+    do_byte[1] += 4 
+
+  if do[11].value == 1: 
+    do_byte[1] += 8 
+
+  if do[12].value == 1: 
+    do_byte[1] += 16 
+
+  if do[13].value == 1: 
+    do_byte[1] += 32 
+
+  if do[14].value == 1: 
+    do_byte[1] += 64 
+
+  if do[15].value == 1: 
+    do_byte[1] += 128 
+
+  #hw_do.write_byte(do[8].do_address, (255 - do_byte[1])) 
+
+  do_byte[2] = 0 
+
+  if do[16].value == 1: 
+    do_byte[2] += 1 
+
+  if do[17].value == 1 : 
+    do_byte[2] += 2 
+
+  if do[18].value == 1: 
+    do_byte[2] += 4 
+
+  if do[19].value == 1: 
+    do_byte[2] += 8 
+
+  if do[20].value == 1: 
+    do_byte[2] += 16 
+
+  if do[21].value == 1: 
+    do_byte[2] += 32 
+
+  if do[22].value == 1: 
+    do_byte[2] += 64 
+
+  if do[23].value == 1: 
+    do_byte[2] += 128 
+
+  #hw_do.write_byte(do[16].do_address, (255 - do_byte[2])) 
+
+  do_byte[3] = 0 
+
+  if do[24].value == 1: 
+    do_byte[3] += 1 
+
+  if do[25].value == 1 : 
+    do_byte[3] += 2 
+
+  if do[26].value == 1: 
+    do_byte[3] += 4 
+
+  if do[27].value == 1: 
+    do_byte[3] += 8 
+
+  if do[28].value == 1: 
+    do_byte[3] += 16 
+
+  if do[29].value == 1: 
+    do_byte[3] += 32 
+
+  if do[30].value == 1: 
+    do_byte[3] += 64 
+
+  if do[31].value == 1: 
+    do_byte[3] += 128 
+
+  #hw_do.write_byte(do[8].do_address, (255 - do_byte[3])) 
 
   return 0 
 
